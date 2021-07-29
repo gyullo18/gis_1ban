0ad22900981686eaeb2ab0f5f2ec940c27148a0e
gyullo18 <gold798@naver.com>
2021-07-29 15:17:51 +0900

    7/29 1ban class commit 3

--- gis_1/urls.py
+++ gis_1/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
     path('profiles/', include('profileapp.urls')),
+    path('articles/', include('articleapp.urls')),
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) #미디어 루트에서 스태틱 추가 7/26
