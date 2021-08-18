9e57840b53fdc4fa81ac0f8c4f976332ba36af0b
gyullo18 <gold798@naver.com>
2021-08-18 10:14:39 +0900

    8/18 1ban class commit 4

--- articleapp/forms.py
+++ articleapp/forms.py
@@ -7,6 +7,6 @@ from articleapp.models import Article
 class ArticleCreationForm(ModelForm):
     class Meta:
         model = Article
-        fields = ['title', 'image', 'content']
-
+        fields = ['title', 'image', 'project', 'content']
+        # 8/18 project추가 -- 어떤 게시판으로 이동할지
         #view로
