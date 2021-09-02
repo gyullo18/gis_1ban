446631392b5991635a3ae9e65437ced860a8ee06
gyullo18 <gold798@naver.com>
2021-09-02 17:20:50 +0900

    9/2 1ban class commit 2

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,16 +1,14 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accountapp.views import  AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
+from accountapp.views import AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
 
 app_name = 'accountapp'
 
 urlpatterns = [
-    path('Login/', LoginView.as_view(template_name='accountapp/Login.html'),
-         name='login'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
 
-    path('Logout/', LogoutView.as_view(template_name='accountapp/Logout.html'),
-         name='logout'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/', AccountCreateView.as_view(), name='create'),#클래스를 함수로 뱉어주는 메서드 #라우트
 
